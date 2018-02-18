95ecbc3fc6bcd9e66d387a4efe37afa8e7d69253
Aurelijus <aurelijus@banelis.lt>
2018-02-18 17:35:26 +0000

    Simple Twig example

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -1,17 +1,19 @@
 <?php
-// src/Controller/LuckyController.php
+
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class LuckyController
+class LuckyController extends Controller
 {
     public function number()
     {
         $number = mt_rand(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('lucky/number.html.twig', array(
+            'number' => $number,
+        ));
+
+//        (new \DateTime())->format('Y-m-d');
     }
 }
