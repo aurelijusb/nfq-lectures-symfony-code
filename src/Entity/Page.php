a8954426572efacba4b1036a87dde0936c3f3ceb
Aurelijus Banelis <aurelijus@banelis.lt>
2018-03-24 15:57:56 +0000

    Adding some fields to Page Entity
    Using PHPStorm generators for Constructor and Setter/Getters.

--- src/Entity/Page.php
+++ src/Entity/Page.php
@@ -13,8 +13,56 @@ class Page
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @var int
      */
     private $id;
 
-    // add your own fields
+    /**
+     * @ORM\Column(type="text")
+     * @var string
+     */
+    private $content;
+
+    /**
+     * Page constructor.
+     * @param int $id
+     * @param string $content
+     */
+    public function __construct(int $id, string $content)
+    {
+        $this->id = $id;
+        $this->content = $content;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
+    }
 }
