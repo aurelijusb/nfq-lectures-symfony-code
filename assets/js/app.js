7c9e028fae1265ba8542029e5191be04e0e01c56
Aurelijus Banelis <aurelijus@banelis.lt>
2018-03-30 19:50:15 +0000

    Frontend: Updating template to use Bootstrap
    "~" in SCSS works as a link to node_modules.
    Adding CSS and JavaScript dependencies in assets/ files.
    
    Updated Twig templates to use HTML tag names and class names for site to look more beautiful (and using bootstrap).
    
    For more documentation – see Bootstrap 4.0.0 components.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,6 @@
-var element = document.createElement('div');
+var element = document.createElement('footer');
+element.className="page-footer font-small bg-dark text-light mt-4 text-center";
 element.innerHTML = "&copy; " + (new Date()).getFullYear();
 document.body.appendChild(element);
+
+require('bootstrap');
